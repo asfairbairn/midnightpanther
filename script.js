9574ce534ade6f884e0930e1de1616061e39d3a2
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-10-24 11:47:10 -0500

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,20 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h3);
+
+const h4 = document.createElement("h4");
+h4.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h4);
+
+const h5 = document.createElement("h5");
+h5.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h5);
+
+const h6 = document.createElement("h6");
+h6.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h6);
+
